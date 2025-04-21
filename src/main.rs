43fa66f8d635594bf0ec81f299ef83c9790978c7
Mechardo <lucas_rack@live.com.ar>
2025-04-21 01:10:58 -0300

    task de tokio

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,14 @@ use std::time::Duration;
 use thirtyfour::prelude::*;
 use thirtyfour::{By, WebDriver};
 use tokio;
-use tokio::time;
-use tokio::time::sleep;
 use dotenv::dotenv;
+use tokio::{sync::broadcast};
 
 #[tokio::main]
 async fn main() {
     dotenv().ok();
     let mut mqttoptions = MqttOptions::new("test-client2", "192.168.100.2", 1883);
     mqttoptions.set_keep_alive(Duration::from_secs(5));
-    println!("Inicio");
 
     // Create the client and event loop
     let (client, mut eventloop) = AsyncClient::new(mqttoptions, 10);
@@ -23,14 +21,17 @@ async fn main() {
         return;
     }
 
-    println!("Suscrito al topico");
+    if let Err(e) = client.subscribe("aidot/get/cam2", QoS::AtLeastOnce).await {
+        eprintln!("Failed to subscribe: {:?}", e);
+        return;
+    }
 
     let user = var("AIDOT_USER").expect("Missing AIDOT_USER in environment");
     let pass = var("AIDOT_PASSWORD").expect("Missing AIDOT_PASSWORD in environment");
 
-    // Inicializar WebDriver (Chrome)
     let mut caps = DesiredCapabilities::chrome();
 
+    /* chrome args */
     caps.add_chrome_arg("--headless").unwrap();
     caps.add_chrome_arg("--no-sandbox").unwrap();
     caps.add_chrome_arg("--disable-setuid-sandbox").unwrap();
@@ -47,76 +48,71 @@ async fn main() {
     caps.add_chrome_arg("--disable-features=IsolateOrigins,site-per-process")
         .unwrap();
 
-    // caps.add_chrome_option("profile.default_content_setting_values.media_stream_mic", json!(1)).unwrap();
+    let (tx, _) = broadcast::channel::<(String, Vec<u8>)>(32);
 
-    
-
-    println!("Esperando mensajes...");
-
-    loop {
-        match eventloop.poll().await {
-            Ok(Event::Incoming(Incoming::ConnAck(_))) => {
-                println!("Connected to broker!");
+    // Task de recepción MQTT
+    let tx_clone = tx.clone();
+    tokio::spawn(async move {
+        loop {
+            match eventloop.poll().await {
+                Ok(Event::Incoming(Incoming::ConnAck(_))) => {
+                    println!("Connected to broker!");
+                }
+                Ok(Event::Incoming(Incoming::Publish(p))) => {
+                    let _ = tx_clone.send((p.topic.clone(), p.payload.to_vec()));
+                }
+                Ok(_) => {}
+                Err(_e) => {}
             }
-            Ok(Event::Incoming(Incoming::Publish(_p))) => {
-                let driver = WebDriver::new("http://localhost:9515", caps.clone()).await.unwrap();
-                
-                // Ir a la página de login
-                driver.goto("https://app.aidot.com").await.unwrap();
+        }
+    });
 
-                let current_url = driver.current_url().await.unwrap().to_string();
-                if current_url.contains("/SignIn") {
-                    // Rellenar campos de login
-                    let username = driver
-                        .query(By::Css("input[placeholder='User Name']"))
-                        .first()
-                        .await
-                        .unwrap();
-                    username.send_keys(&user).await.unwrap();
+    // task 1
+    let mut rx_a = tx.subscribe();
+    tokio::spawn(async move {
+        let driver = WebDriver::new("http://localhost:9515", caps.clone()).await.unwrap();
+        driver.goto("https://app.aidot.com").await.unwrap();
+        let current_url = driver.current_url().await.unwrap().to_string();
+        if current_url.contains("/SignIn") {
+            // Rellenar campos de login
+            let username = driver
+                .query(By::Css("input[placeholder='User Name']"))
+                .first()
+                .await
+                .unwrap();
+            username.send_keys(&user).await.unwrap();
 
-                    let password = driver
-                        .query(By::Css("input[placeholder='Password']"))
-                        .first()
-                        .await
-                        .unwrap();
-                    password.send_keys(&pass).await.unwrap();
+            let password = driver
+                .query(By::Css("input[placeholder='Password']"))
+                .first()
+                .await
+                .unwrap();
+            password.send_keys(&pass).await.unwrap();
 
-                    let submit_btn = driver
-                        .query(By::Css("button[type='button'].MuiButton-root"))
-                        .first()
-                        .await
-                        .unwrap();
-                    submit_btn.click().await.unwrap();
-                }
+            let submit_btn = driver
+                .query(By::Css("button[type='button'].MuiButton-root"))
+                .first()
+                .await
+                .unwrap();
+            submit_btn.click().await.unwrap();
+        }
+        println!("✅ Login exitoso");
 
-                // Esperar que cambie la URL (hasta 20s) o esperar algún elemento post-login
-                let mut retries = 20;
-                loop {
-                    let current_url = driver.current_url().await.unwrap().to_string();
-                    if !current_url.contains("/SignIn") {
-                        break;
-                    }
-                    if retries == 0 {
-                        panic!("Timeout esperando redirección después del login.");
-                    }
-                    retries -= 1;
-                    tokio::time::sleep(Duration::from_secs(1)).await;
-                }
+        // Ir a la vista de la cámara
+        // Suponiendo que `driver` ya está inicializado
+        driver
+            .goto("https://app.aidot.com/live/b228e9b618e541479f9b4a636bdf52e0")
+            .await
+            .unwrap();
 
-                println!("✅ Login exitoso");
+        // Esperar a que el video tenga dimensiones válidas
+        println!("⏳ Esperando a que el video cargue...");
+        let video_element = wait_for_video(&driver).await.unwrap();
 
-                // Ir a la vista de la cámara
-                // Suponiendo que `driver` ya está inicializado
-                driver
-                    .goto("https://app.aidot.com/live/b228e9b618e541479f9b4a636bdf52e0")
-                    .await
-                    .unwrap();
-
-                // Esperar a que el video tenga dimensiones válidas
-                println!("⏳ Esperando a que el video cargue...");
-                let video_element = wait_for_video(&driver).await.unwrap();
-
-                match video_element {
+        while let Ok((topic, payload)) = rx_a.recv().await {
+            if topic == "topic/a" {
+                println!("Task A: {:?}", String::from_utf8_lossy(&payload));
+                match &video_element {
                     Some(video_elem) => {
                         println!("✅ Video cargado con dimensiones válidas");
 
@@ -131,18 +127,100 @@ async fn main() {
                         println!("❌ No se pudo cargar el video con dimensiones válidas");
                     }
                 }
-
-                // Cerrar el navegador
-                driver.quit().await.unwrap();
-            }
-            Ok(_) => {}
-            Err(e) => {
-                eprintln!("Easdrror: {:?}", e);
-                time::sleep(Duration::from_secs(5)).await;
-                // Add reconnection logic if needed
             }
         }
-    }
+    });
+
+    // loop {
+    //     match eventloop.poll().await {
+    //         Ok(Event::Incoming(Incoming::ConnAck(_))) => {
+    //             println!("Connected to broker!");
+    //         }
+    //         Ok(Event::Incoming(Incoming::Publish(_p))) => {
+    //             let driver = WebDriver::new("http://localhost:9515", caps.clone()).await.unwrap();
+                
+    //             // Ir a la página de login
+    //             driver.goto("https://app.aidot.com").await.unwrap();
+
+    //             let current_url = driver.current_url().await.unwrap().to_string();
+    //             if current_url.contains("/SignIn") {
+    //                 // Rellenar campos de login
+    //                 let username = driver
+    //                     .query(By::Css("input[placeholder='User Name']"))
+    //                     .first()
+    //                     .await
+    //                     .unwrap();
+    //                 username.send_keys(&user).await.unwrap();
+
+    //                 let password = driver
+    //                     .query(By::Css("input[placeholder='Password']"))
+    //                     .first()
+    //                     .await
+    //                     .unwrap();
+    //                 password.send_keys(&pass).await.unwrap();
+
+    //                 let submit_btn = driver
+    //                     .query(By::Css("button[type='button'].MuiButton-root"))
+    //                     .first()
+    //                     .await
+    //                     .unwrap();
+    //                 submit_btn.click().await.unwrap();
+    //             }
+
+    //             // Esperar que cambie la URL (hasta 20s) o esperar algún elemento post-login
+    //             let mut retries = 20;
+    //             loop {
+    //                 let current_url = driver.current_url().await.unwrap().to_string();
+    //                 if !current_url.contains("/SignIn") {
+    //                     break;
+    //                 }
+    //                 if retries == 0 {
+    //                     panic!("Timeout esperando redirección después del login.");
+    //                 }
+    //                 retries -= 1;
+    //                 tokio::time::sleep(Duration::from_secs(1)).await;
+    //             }
+
+    //             println!("✅ Login exitoso");
+
+    //             // Ir a la vista de la cámara
+    //             // Suponiendo que `driver` ya está inicializado
+    //             driver
+    //                 .goto("https://app.aidot.com/live/b228e9b618e541479f9b4a636bdf52e0")
+    //                 .await
+    //                 .unwrap();
+
+    //             // Esperar a que el video tenga dimensiones válidas
+    //             println!("⏳ Esperando a que el video cargue...");
+    //             let video_element = wait_for_video(&driver).await.unwrap();
+
+    //             match video_element {
+    //                 Some(video_elem) => {
+    //                     println!("✅ Video cargado con dimensiones válidas");
+
+    //                     // Tomar captura de pantalla solo del elemento <video>
+    //                     println!("📸 Tomando captura del elemento <video>...");
+    //                     let screenshot = video_elem.screenshot_as_png().await.unwrap();
+    //                     std::fs::write("/home/lucas/home-assistant/data/captura_video.png", screenshot)
+    //                         .unwrap();
+    //                     println!("✅ Captura guardada como captura_video.png");
+    //                 }
+    //                 None => {
+    //                     println!("❌ No se pudo cargar el video con dimensiones válidas");
+    //                 }
+    //             }
+
+    //             // Cerrar el navegador
+    //             //driver.quit().await.unwrap();
+    //         }
+    //         Ok(_) => {}
+    //         Err(e) => {
+    //             eprintln!("Easdrror: {:?}", e);
+    //             time::sleep(Duration::from_secs(5)).await;
+    //             // Add reconnection logic if needed
+    //         }
+    //     }
+    // }
 }
 
 // Función para esperar a que el video tenga dimensiones válidas y devolver el elemento
@@ -166,8 +244,8 @@ async fn wait_for_video(driver: &WebDriver) -> WebDriverResult<Option<thirtyfour
                 println!("Error al ejecutar script: {:?}", e);
             }
         }
-        sleep(Duration::from_secs(1)).await;
     }
 
     Ok(None)
 }
+
